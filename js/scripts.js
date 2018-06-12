1a60d17657ebe05ca053a6893979a3c66450ffe5
“Nick <amellocello@msn.com>
2018-06-11 21:47:13 -0700

    Add ability to remove elements from list.

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,36 @@ function Task (task, time, description) {
   this.time = time;
   this.description = description;
 };
-
+var currentTask = "";
 $(document).ready(function(){
   $("#form-to-do").submit(function(event){
     event.preventDefault();
     var inputtedTask = $("#new-task").val();
     var inputtedTime = $("#new-time").val();
     var inputtedDescription = $("#new-description").val();
+    if (inputtedTask !== "") {
+      var newTask = new Task (inputtedTask, inputtedTime, inputtedDescription);
 
-    var newTask = new Task (inputtedTask, inputtedTime, inputtedDescription);
+      $("#task-list").append("<li><span class='task-element'>" + newTask.task + "</span> " + "<a class='remove'>Remove</a></li>");
 
-    $("#task-list").append("<li class='task-element'>" + newTask.task + "</li>");
+      $(".task-element").last().click(function() {
+        $("#task-info").show();
+        $("#task-info h2").text(newTask.task);
+        $(".time-input").text(newTask.time);
+        $(".description-input").text(newTask.description);
+        currentTask = newTask.task;
+        alert(currentTask);
+      });
 
-    $(".task-element").last().click(function() {
-      $("#task-info").show();
-      $("#task-info h2").text(newTask.task);
-      $(".time-input").text(newTask.time);
-      $(".description-input").text(newTask.description);
-    });
+      $(".remove").click(function(){
+        $(this).parent().remove();
+        if (currentTask === newTask.task){
+          $("#task-info").hide();
+        }
+      })
+      $("#new-task").val("");
+      $("#new-time").val("");
+      $("#new-description").val("");
+    }
   });
 });
